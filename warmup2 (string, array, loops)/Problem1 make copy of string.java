f522aaca0c2e9c3d9c41bd73d334f337180b2a97
lakhan <oplopr01@gmail.com>
2023-09-29 16:48:35 +0530

    copy string n times problem

--- warmup2 (string, array, loops)/Problem1 make copy of string.java
+++ warmup2 (string, array, loops)/Problem1 make copy of string.java
@@ -1,6 +1,11 @@
 
 	-----------copy given string n times and create new string by it----------
 
+	
+Problem-: Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+
 public String stringTimes(String str, int n) {
   String temp="";
   for(int i =0;i<n;i++){
