7f2e4bf2778244d76d6e6f6e1f1f8d40ad905d42
lakhan <oplopr01@gmail.com>
2023-12-21 08:40:24 +0530

    Signed-off-by: lakhan

---
+++ warmup2 (string, array, loops)/15.repeat char as next char digit.java
@@ -0,0 +1,11 @@
+
+
+// print previous char as many time next char | change a3b2d2a1 into aaabbdda
+	static void repeatCharCountNextChar(String s) {
+		for(int i =0;i<s.length()-1;i=i+2) {
+			char c = s.charAt(i);
+			int num = s.charAt(i+1)-48;
+			
+		}
+		
+	}
